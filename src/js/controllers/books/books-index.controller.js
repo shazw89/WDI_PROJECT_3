bdfc406e4ee06698e618154cdfb39f70d12ab65b
HenryDavies <henrywdavies@gmail.com>
2017-02-01 23:29:17 +0000

    added map to book index page

--- src/js/controllers/books/books-index.controller.js
+++ src/js/controllers/books/books-index.controller.js
@@ -1,13 +1,47 @@
 angular
-  .module('angularAuthentication')
-  .controller('BooksIndexCtrl', BooksIndexCtrl);
+.module('angularAuthentication')
+.controller('BooksIndexCtrl', BooksIndexCtrl);
 
 BooksIndexCtrl.$inject = ['Book'];
 function BooksIndexCtrl(Book){
   const vm = this;
+  const infoWindows = [];
 
   Book.query().$promise
   .then(response => {
     vm.books = response;
+    for (const book of vm.books) {
+      for (const entry of book.entries) {
+        const latlng = new google.maps.LatLng(entry.lat, entry.lng);
+        const marker = new google.maps.Marker({
+          position: latlng,
+          map: vm.map
+        });
+        vm.markers.push(marker);
+        google.maps.event.addListener(marker, 'click', () => {
+          const infoWindow = new google.maps.InfoWindow({
+            content: `
+            <h6>${book.title}</h6>
+            <p>${entry.name}</p>
+            `
+          });
+          if (infoWindows[0]) infoWindows[0].close();
+          infoWindows.pop();
+          infoWindow.open(vm.map, marker);
+          infoWindows.push(infoWindow);
+        });
+      }
+    }
   });
+
+  const mapOptions = {
+    zoom: 11,
+    center: new google.maps.LatLng(51.5085300,-0.1257400),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  vm.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  vm.markers = [];
+
 }
