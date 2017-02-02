237d8d2b17e0eee759d503c87aae8600f064c88d
chrisofarabia <chrisofarabia@yahoo.co.uk>
2017-02-02 14:39:04 +0000

    Map and markers added to the showBook page

--- src/js/controllers/books/books-show.controller.js
+++ src/js/controllers/books/books-show.controller.js
@@ -5,14 +5,47 @@ angular
 BooksShowCtrl.$inject = ['$stateParams', 'Book', '$state', '$http'];
 function BooksShowCtrl($stateParams, Book, $state, $http){
   const vm = this;
+  const infoWindows = [];
   vm.found = $stateParams.found;
 
   Book
     .get({ shortId: $stateParams.shortId }).$promise
     .then((response) => {
       vm.book = response;
-      // console.log(vm.book);
+      console.log(vm.book);
+      // for (const book of vm.books) {
+        for (const entry of vm.book.entries) {
+          const latlng = new google.maps.LatLng(entry.lat, entry.lng);
+          const marker = new google.maps.Marker({
+            position: latlng,
+            map: vm.map
+          });
+          vm.markers.push(marker);
+          google.maps.event.addListener(marker, 'click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+              content: `
+              <h6>${vm.book.title}</h6>
+              <p>${entry.name}</p>
+              `
+            });
+            if (infoWindows[0]) infoWindows[0].close();
+            infoWindows.pop();
+            infoWindow.open(vm.map, marker);
+            infoWindows.push(infoWindow);
+          });
+        }
+      // }
     });
+
+    const mapOptions = {
+      zoom: 11,
+      center: new google.maps.LatLng(51.5085300,-0.1257400),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    vm.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    vm.markers = [];
 
   vm.updateEntries = updateEntries;
 
