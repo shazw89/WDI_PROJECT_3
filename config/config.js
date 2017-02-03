3d61686e15edf10430e07203bbdccd6e99ba2bdf
EffingKay <rozgonyiova.k@gmail.com>
2017-02-03 12:14:33 +0000

    heroku setting

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const secret = process.env.SECRET || 'Something very very secret...';
 const port = process.env.PORT || 3000;
-const db = 'mongodb://localhost/travellingBooks';
+const db = process.env.MONGODB_URI || 'mongodb://localhost/travellingBooks';
 
 module.exports = {
   secret,
